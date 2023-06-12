7ed2a3833e633b0be0c20ecafbd7d6d327f5e0a8
UGO <ugobourgoinscarfo@gmail.com>
2023-06-12 11:34:08 +0200

    maj express_quests

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = process.env.APP_PORT || 5000;
+
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favorite movie list");
+};
+
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+
+app.get("/", welcome);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
